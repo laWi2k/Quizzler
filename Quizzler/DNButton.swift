529ec59f34afad8818b2a6f6099713c5d950d9db
Daniel <neskdan812@gmail.com>
2022-07-11 17:52:12 +0300

    Initial commit

---
+++ Quizzler/DNButton.swift
@@ -0,0 +1,29 @@
+//
+//  DNButton.swift
+//  Quizzler
+//
+//  Created by Daniel on 11.07.2022.
+//
+
+import Foundation
+import UIKit
+
+class DNButton: UIButton {
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    convenience init (title: String){
+        self.init()
+        setTitle(title, for: .normal)
+        backgroundColor = .white
+        layer.cornerRadius = 16
+        titleLabel?.font = .boldSystemFont(ofSize: 30)
+        setTitleColor(.black, for: .normal)
+        sizeToFit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -7,13 +7,146 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UIStackView {
+    func addArrangedSubviews(_ subviews: [UIView]) {
+        subviews.forEach { view in
+            addArrangedSubview(view)
+        }
+    }
+}
 
+struct QuestionsForQuiz {
+    var question: String
+    var correctAnswer: Bool
+    var isUsedBefore: Bool
+    
+    static let questions = [QuestionsForQuiz(question: "There are 4 Maxwell's equations, right?", correctAnswer: true, isUsedBefore: false),
+                       QuestionsForQuiz(question: "Is Russia the biggest country?", correctAnswer: true, isUsedBefore: false),
+                       QuestionsForQuiz(question: "Did Max Verstappen win the 2021 FIA F1 Championship?", correctAnswer: true, isUsedBefore: false),
+                       QuestionsForQuiz(question: "Did Rayan Gosling die at the end of 'Drive'?", correctAnswer: false, isUsedBefore: false),
+                       QuestionsForQuiz(question: "Did Red Bull win the 2021 FIA F1 Constructors Championship?", correctAnswer: false, isUsedBefore: false),
+                       QuestionsForQuiz(question: "Am i gonna be an iOS developer?", correctAnswer: true, isUsedBefore: false),
+                       QuestionsForQuiz(question: "Is Moscow the capital of Russia?", correctAnswer: true, isUsedBefore: false)]
+}
+
+
+class ViewController: UIViewController {
+    
+    //MARK: creating things
+    
+    let buttonTrue = DNButton(title: "True")
+    let buttonFalse = DNButton(title: "False")
+    
+    let labelView: UILabel = {
+        let questionLabel = UILabel()
+        questionLabel.text = "Question Text"
+        questionLabel.textColor = .black
+        questionLabel.textAlignment = .center
+        questionLabel.font = .boldSystemFont(ofSize: 25)
+        questionLabel.layer.cornerRadius = 16
+        questionLabel.numberOfLines = 0
+        return questionLabel
+    }()
+    
+    let progressView: UIProgressView = {
+        let progressBar = UIProgressView(progressViewStyle: .bar)
+        progressBar.setProgress(0.5, animated: true)
+        progressBar.trackTintColor = .white
+        progressBar.tintColor = .black
+        progressBar.transform = progressBar.transform.scaledBy(x: 1, y: 5)
+        progressBar.layer.cornerRadius = 10
+        return progressBar
+    }()
+    
+    let vStackView = UIStackView()
+    
+    //MARK: Quiz logic
+    
+    var questionNumber = 0
+    
+    func updateUILabel() {
+        
+        guard let question = QuestionsForQuiz.questions.randomElement(), !question.isUsedBefore else {
+            return
+        }
+        
+        
+        labelView.text = question.question
+        //labelView.text = Array(quizQuestions.keys)[questionNumber]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBlue
+        view.addSubview(vStackView)
+        constraints()
+        updateUILabel()
+        
+        vStackView.addArrangedSubviews([
+            labelView,
+            buttonTrue,
+            buttonFalse,
+            progressView,
+            
+        ])
+        
+        vStackView.spacing = 15
+        vStackView.axis = .vertical
+        vStackView.clipsToBounds = true
+        vStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        targeting()
+        
     }
-
-
+    
+    //MARK: Setting constraints
+    func constraints(){
+        NSLayoutConstraint.activate([
+            vStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            vStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            vStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            vStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+    }
+    
+    
+    //MARK: Buttons
+    @objc func keyFalsePressedDown(){
+        buttonFalse.alpha = 0.5
+        if questionNumber + 1 < QuestionsForQuiz.questions.count {
+                    questionNumber += 1
+                } else {
+                    questionNumber = 0
+                }
+                updateUILabel()
+        
+//                print("false pressed")
+    }
+    
+    @objc func keyFalsePressedUp(){
+        buttonFalse.alpha = 1
+        
+    }
+    
+    @objc func keyTruePressedDown(){
+        buttonTrue.alpha = 0.5
+        
+    }
+    
+    @objc func keyTruePressedUp(){
+        buttonTrue.alpha = 1
+        
+    }
+    
+    func targeting() {
+        buttonTrue.addTarget(self, action: #selector(keyTruePressedDown), for: .touchDown)
+        buttonTrue.addTarget(self, action: #selector(keyTruePressedUp), for: .touchUpInside)
+        buttonFalse.addTarget(self, action: #selector(keyFalsePressedDown), for: .touchDown)
+        buttonFalse.addTarget(self, action: #selector(keyFalsePressedUp), for: .touchUpInside)
+    }
+    
+    
+    
 }
 
