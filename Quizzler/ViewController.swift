e745fb9d722b0699758685e4b5d43cd8525ad668
Daniel <neskdan812@gmail.com>
2022-07-16 13:15:21 +0300

    small code refactoring

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+
+//extedning StackView so it can add multiple subviews
 extension UIStackView {
     func addArrangedSubviews(_ subviews: [UIView]) {
         subviews.forEach { view in
@@ -15,22 +17,25 @@ extension UIStackView {
     }
 }
 
+
 struct QuestionsForQuiz {
     var question: String
-    var correctAnswer: Bool
+    var correctAnswer: String
     var isUsedBefore: Bool
     
-    static let questions = [QuestionsForQuiz(question: "There are 4 Maxwell's equations, right?", correctAnswer: true, isUsedBefore: false),
-                       QuestionsForQuiz(question: "Is Russia the biggest country?", correctAnswer: true, isUsedBefore: false),
-                       QuestionsForQuiz(question: "Did Max Verstappen win the 2021 FIA F1 Championship?", correctAnswer: true, isUsedBefore: false),
-                       QuestionsForQuiz(question: "Did Rayan Gosling die at the end of 'Drive'?", correctAnswer: false, isUsedBefore: false),
-                       QuestionsForQuiz(question: "Did Red Bull win the 2021 FIA F1 Constructors Championship?", correctAnswer: false, isUsedBefore: false),
-                       QuestionsForQuiz(question: "Am i gonna be an iOS developer?", correctAnswer: true, isUsedBefore: false),
-                       QuestionsForQuiz(question: "Is Moscow the capital of Russia?", correctAnswer: true, isUsedBefore: false)]
+    static var questions = [QuestionsForQuiz(question: "There are 4 Maxwell's equations, right?", correctAnswer: "true", isUsedBefore: false),
+                            QuestionsForQuiz(question: "Is Russia the biggest country?", correctAnswer: "true", isUsedBefore: false),
+                            QuestionsForQuiz(question: "Did Max Verstappen win the 2021 FIA F1 Championship?", correctAnswer: "true", isUsedBefore: false),
+                            QuestionsForQuiz(question: "Did Rayan Gosling die at the end of 'Drive'?", correctAnswer: "false", isUsedBefore: false),
+                            QuestionsForQuiz(question: "Did Red Bull win the 2021 FIA F1 Constructors Championship?", correctAnswer: "false", isUsedBefore: false),
+                            QuestionsForQuiz(question: "Am i gonna be an iOS developer?", correctAnswer: "true", isUsedBefore: false),
+                            QuestionsForQuiz(question: "Is Moscow the capital of Russia?", correctAnswer: "true", isUsedBefore: false)]
 }
 
 
 class ViewController: UIViewController {
+    
+    var currentQuestions = QuestionsForQuiz.questions
     
     //MARK: creating things
     
@@ -54,25 +59,30 @@ class ViewController: UIViewController {
         progressBar.trackTintColor = .white
         progressBar.tintColor = .black
         progressBar.transform = progressBar.transform.scaledBy(x: 1, y: 5)
-        progressBar.layer.cornerRadius = 10
+        //        progressBar.layer.cornerRadius = 10
         return progressBar
     }()
     
     let vStackView = UIStackView()
     
-    //MARK: Quiz logic
+    var currentIndex: Int?
     
-    var questionNumber = 0
     
-    func updateUILabel() {
+    @objc func updateUILabel() {
         
-        guard let question = QuestionsForQuiz.questions.randomElement(), !question.isUsedBefore else {
-            return
-        }
-        
+        guard let question = currentQuestions.randomElement(), !question.isUsedBefore else {return}
         
         labelView.text = question.question
-        //labelView.text = Array(quizQuestions.keys)[questionNumber]
+        
+        
+        for index in 0..<currentQuestions.count{
+            if currentQuestions[index].question == question.question {
+                currentQuestions[index].isUsedBefore = true
+                currentIndex = index
+//                print(currentQuestions.count)
+//                currentQuestions.remove(at: сurrentIndex)
+            }
+        }
     }
     
     override func viewDidLoad() {
@@ -114,18 +124,20 @@ class ViewController: UIViewController {
     //MARK: Buttons
     @objc func keyFalsePressedDown(){
         buttonFalse.alpha = 0.5
-        if questionNumber + 1 < QuestionsForQuiz.questions.count {
-                    questionNumber += 1
-                } else {
-                    questionNumber = 0
-                }
-                updateUILabel()
         
-//                print("false pressed")
     }
     
     @objc func keyFalsePressedUp(){
         buttonFalse.alpha = 1
+        
+        if "false" == currentQuestions[currentIndex!].correctAnswer.lowercased() /*&& !currentQuestions.isEmpty */{
+            buttonTrue.backgroundColor = .white
+            buttonFalse.backgroundColor = .white
+            updateUILabel()
+//            currentQuestions.remove(at: currentIndex!)
+        } else {
+            buttonFalse.backgroundColor = .systemRed
+        }
         
     }
     
@@ -136,6 +148,16 @@ class ViewController: UIViewController {
     
     @objc func keyTruePressedUp(){
         buttonTrue.alpha = 1
+        if "true" == currentQuestions[currentIndex!].correctAnswer.lowercased() /*&& !currentQuestions.isEmpty */ {
+            buttonTrue.backgroundColor = .white
+            buttonFalse.backgroundColor = .white
+//            currentQuestions.remove(at: currentIndex!)
+            _ = Timer(timeInterval: 0.5, target: self, selector: #selector(updateUILabel), userInfo: nil, repeats: true)
+            updateUILabel()
+        } else {
+            buttonTrue.backgroundColor = .systemRed
+        }
+
         
     }
     
@@ -145,8 +167,6 @@ class ViewController: UIViewController {
         buttonFalse.addTarget(self, action: #selector(keyFalsePressedDown), for: .touchDown)
         buttonFalse.addTarget(self, action: #selector(keyFalsePressedUp), for: .touchUpInside)
     }
-    
-    
     
 }
 
